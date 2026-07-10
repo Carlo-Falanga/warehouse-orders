88e82b9053f1126403367140a625be8af1e9b59a
Carlo-Falanga <carlofalanga7@gmail.com>
2026-07-10 12:54:00 +0200

    feat: initialize server with Express and setup middleware

---
+++ server/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+import "./db/index.js";
+import ordersRouter from "./routes/orders.routes.js";
+import {
+  errorHandler,
+  notFoundHandler,
+} from "./middleware/error.middleware.js";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware globali
+app.use(cors());
+app.use(express.json());
+
+// Helth check del server
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", timestamp: new Date().toISOString() });
+});
+
+// Route
+app.use("/orders", ordersRouter);
+
+// Middleware per la gestione errori
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
